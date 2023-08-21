078151f9f9d644609783339f7d12dcc941e838bc
job monari <jobmonari7@gmail.com>
2023-08-22 02:57:00 +0300

    adding the tweet logic

--- [1] Prisma Node and Typescript/src/routes/tweetRoutes.ts
+++ [1] Prisma Node and Typescript/src/routes/tweetRoutes.ts
@@ -1,22 +1,37 @@
 import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
 
 const router = Router();
+const prisma = new PrismaClient();
 //user CRUD Content
 
 //create Tweet
-router.post('/', (req, res) => {
-  res.status(501).json({ error: "Not Implemented" })
+router.post('/', async (req, res) => {
+  const { content, image } = req.body
+  try {
+    const result = await prisma.tweet.create({
+      data: {
+        content,
+        image
+      }
+    })
+    res.json(result)
+  } catch (e) {
+    res.status(400).json({ error: "cannot create tweet" })
+  }
 })
 
 //list of Tweets
-router.get('/', (req, res) => {
-  res.status(501).json({ error: "Not Implemented" })
+router.get('/', async (req, res) => {
+  const result = await prisma.tweet.findMany()
+  res.json(result)
 })
 
 //get one Tweet
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   const { id } = req.params
-  res.status(501).json({ error: `Not Implemented: ${id}` })
+  const result = prisma.tweet.findUnique({ where: { id: Number(id) } })
+  res.json(result)
 })
 
 //update Tweet
